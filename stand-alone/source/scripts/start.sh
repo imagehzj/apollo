4ba0ebbf6d0833d59a8afbefa0261bbc4e226561
Shoothzj <shoothzj@gmail.com>
2021-12-09 21:21:39 +0800

    Support Arm Image

--- stand-alone/source/scripts/start.sh
+++ stand-alone/source/scripts/start.sh
@@ -1,13 +1,15 @@
 mysqld --daemonize --user=root
 
-mysql -u root < /opt/sh/scripts/apollo-config.sql
-mysql -u root < /opt/sh/scripts/apollo-portal.sql
-mysql -u root < /opt/sh/scripts/grant.sql
+sleep 10
 
-cp /opt/sh/conf/apollo-env.properties /opt/sh/apollo-portal/config/apollo-env.properties
+mysql -u root < /opt/sh/apollo/scripts/apollo-config.sql
+mysql -u root < /opt/sh/apollo/scripts/apollo-portal.sql
+mysql -u root < /opt/sh/apollo/scripts/grant.sql
 
-bash -x /opt/sh/apollo-config/hzj/scripts/start-daemon.sh
-bash -x /opt/sh/apollo-admin/hzj/scripts/start-daemon.sh
-bash -x /opt/sh/apollo-portal/hzj/scripts/start-daemon.sh
+cp /opt/sh/apollo/conf/apollo-env.properties /opt/sh/apollo/apollo-portal/config/apollo-env.properties
+
+bash -x /opt/sh/apollo/apollo-config/hzj/scripts/start-daemon.sh
+bash -x /opt/sh/apollo/apollo-admin/hzj/scripts/start-daemon.sh
+bash -x /opt/sh/apollo/apollo-portal/hzj/scripts/start-daemon.sh
 
 tail -f /dev/null
